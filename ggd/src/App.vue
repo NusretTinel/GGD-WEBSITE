fb7180c5755bc7776b7e5c2090beed67b82e977a
nusrettinel <nusret661@outlook.com>
2024-12-30 19:59:17 +0300

    Template

--- ggd/src/App.vue
+++ ggd/src/App.vue
@@ -1,26 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <router-view></router-view>
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {  }
 }
 </script>
 
-<style>
+<style lang="scss">
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0px;
+}
+
+#app {
+  font-family: Roboto, Helvetica, Arial, sans-serif;
+  color: #e5e6e7;
+}
+body {
+  overflow-x: hidden;
 }
 </style>
---
+++ ggd/src/components/PageContent/ContactUs/ContactContent.vue
@@ -0,0 +1,15 @@
+
+<template>
+  <div class="">
+    <h1>Bas</h1>
+  </div>
+</template>
+<script >
+export default {
+  name: 'ContactContent',
+}
+</script>
+
+<style >
+
+</style>
